5fbb292f133cf2679e41d81342efe4ff0c3a4e0b
Alice <alice.cordier39210@gmail.com>
2025-07-17 18:48:37 +0200

    Ajout du module ressource avec schéma et handlers #7

---
+++ back/internal/ressource/model.go
@@ -0,0 +1,16 @@
+package ressource
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Ressource struct {
+	ID         primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty" validate:"required"`
+	Type       string             `bson:"type" json:"type" validate:"required"`
+	URL        string             `bson:"url" json:"url" validate:"required,url"`
+	Name       string             `bson:"name" json:"name" validate:"required"`
+	UploadedBy primitive.ObjectID `bson:"uploaded_by" json:"uploaded_by" validate:"required"`
+	CreatedAt  time.Time          `bson:"created_at" json:"created_at" validate:"required"`
+}
