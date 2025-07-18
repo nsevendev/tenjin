6aa19fab70e98886a027e61cc4355e2c9e69f2bd
Alice <alice.cordier39210@gmail.com>
2025-07-18 08:53:49 +0200

    pas de omitempty #13

--- back/internal/offer/model.go
+++ back/internal/offer/model.go
@@ -9,19 +9,19 @@ import (
 type OfferResponse struct {
 	UserID       primitive.ObjectID `bson:"user_id" json:"user_id" validate:"required"`
 	Status       string             `bson:"status" json:"status" validate:"required"`
-	SharedFields []string           `bson:"shared_fields,omitempty" json:"shared_fields,omitempty"`
-	ReplyDate    *time.Time         `bson:"reply_date,omitempty" json:"reply_date,omitempty"`
+	SharedFields []string           `bson:"shared_fields" json:"shared_fields"`
+	ReplyDate    *time.Time         `bson:"reply_date" json:"reply_date"`
 }
 
 type Offer struct {
-	ID            primitive.ObjectID   `bson:"_id,omitempty" json:"id,omitempty" validate:"required"`
+	ID            primitive.ObjectID   `bson:"_id" json:"id" validate:"required"`
 	RecruiterID   primitive.ObjectID   `bson:"recruiter_id" json:"recruiter_id" validate:"required"`
 	Title         string               `bson:"title" json:"title" validate:"required"`
-	Message       *string              `bson:"message,omitempty" json:"message,omitempty"`
-	AttachmentURL *string              `bson:"attachment_url,omitempty" json:"attachment_url,omitempty"`
-	CandidateIDs  []primitive.ObjectID `bson:"candidate_ids,omitempty" json:"candidate_ids,omitempty"`
+	Message       string               `bson:"message" json:"message"`
+	AttachmentURL *string              `bson:"attachment_url" json:"attachment_url"`
+	CandidateIDs  []primitive.ObjectID `bson:"candidate_ids" json:"candidate_ids"`
 	Status        string               `bson:"status" json:"status" validate:"required,oneof=sent viewed accepted declined"`
-	QuizID        *primitive.ObjectID  `bson:"quiz_id,omitempty" json:"quiz_id,omitempty"`
+	QuizID        *primitive.ObjectID  `bson:"quiz_id" json:"quiz_id"`
 	Responses     []OfferResponse      `bson:"responses" json:"responses" validate:"required,dive"`
-	CreatedAt     *time.Time           `bson:"created_at,omitempty" json:"created_at,omitempty"`
+	CreatedAt     *time.Time           `bson:"created_at" json:"created_at"`
 }
