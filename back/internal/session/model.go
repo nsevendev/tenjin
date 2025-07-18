9fd6e8cd03fe306dce24b75ed515474f6d9638b0
Alice <alice.cordier39210@gmail.com>
2025-07-18 08:47:29 +0200

    Pas de omitempty #5

--- back/internal/session/model.go
+++ back/internal/session/model.go
@@ -7,20 +7,20 @@ import (
 )
 
 type Session struct {
-	ID                primitive.ObjectID   `bson:"_id,omitempty" json:"id,omitempty"`
+	ID                primitive.ObjectID   `bson:"_id" json:"id`
 	FormationID       primitive.ObjectID   `bson:"formation_id" json:"formation_id" validate:"required"`
 	InstituteID       primitive.ObjectID   `bson:"organization_id" json:"organization_id" validate:"required"`
 	Title             string               `bson:"title" json:"title" validate:"required"`
 	StartDate         time.Time            `bson:"start_date" json:"start_date" validate:"required"`
 	EndDate           time.Time            `bson:"end_date" json:"end_date" validate:"required"`
-	Users             []primitive.ObjectID `bson:"users,omitempty" json:"users,omitempty"`
-	Trainers          []primitive.ObjectID `bson:"trainers,omitempty" json:"trainers,omitempty"`
-	CourseIDs         []primitive.ObjectID `bson:"course_ids,omitempty" json:"course_ids,omitempty"`
-	Resources         []primitive.ObjectID `bson:"resources,omitempty" json:"resources,omitempty"`
-	Evaluations       []primitive.ObjectID `bson:"evaluations,omitempty" json:"evaluations,omitempty"`
-	Quizzes           []primitive.ObjectID `bson:"quizzes,omitempty" json:"quizzes,omitempty"`
-	Chats             []primitive.ObjectID `bson:"chats,omitempty" json:"chats,omitempty"`
-	CalendarID        *primitive.ObjectID  `bson:"calendar_id,omitempty" json:"calendar_id,omitempty"`
-	AttendanceSheetID *primitive.ObjectID  `bson:"attendance_sheet_id,omitempty" json:"attendance_sheet_id,omitempty"`
+	Users             []primitive.ObjectID `bson:"users" json:"users"`
+	Trainers          []primitive.ObjectID `bson:"trainers" json:"trainers"`
+	CourseIDs         []primitive.ObjectID `bson:"course_ids" json:"course_ids"`
+	Resources         []primitive.ObjectID `bson:"resources" json:"resources"`
+	Evaluations       []primitive.ObjectID `bson:"evaluations" json:"evaluations"`
+	Quizzes           []primitive.ObjectID `bson:"quizzes" json:"quizzes"`
+	Chats             []primitive.ObjectID `bson:"chats" json:"chats"`
+	CalendarID        *primitive.ObjectID  `bson:"calendar_id" json:"calendar_id"`
+	AttendanceSheetID *primitive.ObjectID  `bson:"attendance_sheet_id" json:"attendance_sheet_id"`
 	CreatedAt         time.Time            `bson:"created_at" json:"created_at"`
 }
