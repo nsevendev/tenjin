49b9739a58ec9e5400b741549985acca54d73433
BoissonMathis <math.boiss@laposte.net>
2025-07-25 16:38:37 +0200

    add create controller for insee package #27

---
+++ back/internal/controller/companycontroller/create.go
@@ -0,0 +1,43 @@
+package companycontroller
+
+import (
+	"fmt"
+	"tenjin/back/internal/insee"
+
+	"github.com/gin-gonic/gin"
+	"github.com/nsevenpack/ginresponse"
+	"github.com/nsevenpack/logger/v2/logger"
+)
+
+// Create godoc
+// @Summary Crée une nouvelle company
+// @Description Permet de créer une nouvelle company
+// @Tags company
+// @Accept json
+// @Produce json
+// @Param company body company.CompanyCreateDto true "Informations de la company à créer"
+// @Success 201 {object} ginresponse.JsonFormatterSwag "Company créée avec succès"
+// @Failure 400 {object} ginresponse.JsonFormatterSwag "Erreur de validation"
+// @Failure 500 {object} ginresponse.JsonFormatterSwag "Erreur interne"
+// @Router /company/create [post]
+func (p *companyController) Create(c *gin.Context) {
+	var companyCreateDto insee.CompanyCreateDto
+	if err := c.ShouldBindJSON(&companyCreateDto); err != nil {
+		logger.Ef("Erreur de validation: %v", err)
+		ginresponse.BadRequest(c, "Erreur de validation", ginresponse.ErrorModel{
+			Message: err.Error(),
+			Type:    "Validation",
+			Detail:  fmt.Sprintf("%v", err),
+		})
+		return
+	}
+
+	_, err := p.companyService.Create(c, companyCreateDto)
+	if err != nil {
+		logger.Ef("%v", err)
+		ginresponse.InternalServerError(c, err.Error(), err.Error())
+		return
+	}
+
+	ginresponse.Created(c, "Company créé avec succès", []string{})
+}
