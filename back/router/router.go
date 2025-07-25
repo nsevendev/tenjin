975ac575b3bfc4e09a392f16067e6e20ed3f5272
BoissonMathis <math.boiss@laposte.net>
2025-07-25 16:21:12 +0200

    init company service in router #27

--- back/router/router.go
+++ back/router/router.go
@@ -1,6 +1,10 @@
 package router
 
 import (
+	"tenjin/back/internal/insee"
+
+	initializer "tenjin/back/internal/db"
+
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -10,4 +14,6 @@ const pathApiV1 = "api/v1"
 
 func Routes(r *gin.Engine) {
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	companyService := insee.NewCompanyService(initializer.Db)
 }
