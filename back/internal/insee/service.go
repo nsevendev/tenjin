70dfe8ae602cc0083cd5f144df1abe6d078806bc
BoissonMathis <math.boiss@laposte.net>
2025-07-25 16:27:42 +0200

    add create function in company service #27

--- back/internal/insee/service.go
+++ back/internal/insee/service.go
@@ -1,6 +1,10 @@
 package insee
 
 import (
+	"context"
+
+	"github.com/nsevenpack/logger/v2/logger"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -9,10 +13,31 @@ type companyService struct {
 }
 
 type CompanyServiceInterface interface {
+	Create(ctx context.Context, companyCreateDto CompanyCreateDto) (*Company, error)
 }
 
 func NewCompanyService(db *mongo.Database) CompanyServiceInterface {
 	return &companyService{
 		collection: db.Collection("company"),
 	}
+}
+
+func (s *companyService) Create(ctx context.Context, companyCreateDto CompanyCreateDto) (*Company, error) {
+	company := &Company{
+		Address:   companyCreateDto.Address,
+		BusinessName: companyCreateDto.BusinessName,
+		City:   companyCreateDto.City,
+		Siret: companyCreateDto.Siret,
+		ZipCode:   companyCreateDto.ZipCode,
+	}
+
+	result, err := s.collection.InsertOne(ctx, company)
+	if err != nil {
+		logger.Ef("Erreur lors de la création de la company : %v", err)
+		return nil, err
+	}
+
+	company.ID = result.InsertedID.(primitive.ObjectID)
+
+	return company, nil
 }
