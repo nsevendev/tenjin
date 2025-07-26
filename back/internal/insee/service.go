bae7a84f5af720762dbb1d2c427735929e2eb403
BoissonMathis <math.boiss@laposte.net>
2025-07-26 18:07:07 +0200

    delete create company func #27

--- back/internal/insee/service.go
+++ back/internal/insee/service.go
@@ -1,10 +1,6 @@
 package insee
 
 import (
-	"context"
-
-	"github.com/nsevenpack/logger/v2/logger"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -13,7 +9,6 @@ type companyService struct {
 }
 
 type CompanyServiceInterface interface {
-	Create(ctx context.Context, companyCreateDto *CompanyCreateDto) (*Company, error)
 }
 
 func NewCompanyService(db *mongo.Database) CompanyServiceInterface {
@@ -22,22 +17,3 @@ func NewCompanyService(db *mongo.Database) CompanyServiceInterface {
 	}
 }
 
-func (s *companyService) Create(ctx context.Context, companyCreateDto *CompanyCreateDto) (*Company, error) {
-	company := &Company{
-		Address:      companyCreateDto.Address,
-		BusinessName: companyCreateDto.BusinessName,
-		City:         companyCreateDto.City,
-		Siret:        companyCreateDto.Siret,
-		ZipCode:      companyCreateDto.ZipCode,
-	}
-
-	result, err := s.collection.InsertOne(ctx, company)
-	if err != nil {
-		logger.Ef("Erreur lors de la création de la company : %v", err)
-		return nil, err
-	}
-
-	company.ID = result.InsertedID.(primitive.ObjectID)
-
-	return company, nil
-}
