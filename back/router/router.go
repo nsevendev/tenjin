1d7f7b92a7f43ac1b1e4ed1064e3fa80b6d0c1c8
BoissonMathis <math.boiss@laposte.net>
2025-07-26 18:08:16 +0200

    delete company from router #27

--- back/router/router.go
+++ back/router/router.go
@@ -1,11 +1,6 @@
 package router
 
 import (
-	"tenjin/back/internal/controller/companycontroller"
-	"tenjin/back/internal/insee"
-
-	initializer "tenjin/back/internal/db"
-
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -15,12 +10,4 @@ const pathApiV1 = "api/v1"
 
 func Routes(r *gin.Engine) {
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-
-	companyService := insee.NewCompanyService(initializer.Db)
-	companyController := companycontroller.NewCompanyController(companyService)
-
-	v1 := r.Group(pathApiV1)
-
-	v1Company := v1.Group("/company")
-	v1Company.POST("/create", companyController.Create)
 }
