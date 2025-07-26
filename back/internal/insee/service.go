3539c8fa23cf6865cbad3b0d614b255ce56fec9a
BoissonMathis <math.boiss@laposte.net>
2025-07-26 18:28:37 +0200

    add secret and key for sirene api in .env #27

--- back/internal/insee/service.go
+++ back/internal/insee/service.go
@@ -1,19 +1,2 @@
 package insee
 
-import (
-	"go.mongodb.org/mongo-driver/mongo"
-)
-
-type companyService struct {
-	collection *mongo.Collection
-}
-
-type CompanyServiceInterface interface {
-}
-
-func NewCompanyService(db *mongo.Database) CompanyServiceInterface {
-	return &companyService{
-		collection: db.Collection("company"),
-	}
-}
-
