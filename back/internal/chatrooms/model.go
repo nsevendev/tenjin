5617e5b878ec3a720b098d569149aaa3d346b899
Ryad.253 <ryad.elm@icloud.com>
2025-07-27 22:28:43 +0200

    ajout des salons (pas terminé) #11

--- back/internal/chatrooms/model.go
+++ back/internal/chatrooms/model.go
@@ -5,7 +5,16 @@ type ChatRoom struct {
 	Name         *string              `bson:"name"`                  // Obligatoire
 	Description  *string              `bson:"description"`           // Obligatoire
 	InstituteID  primitive.ObjectID   `bson:"institute_id"`          // Obligatoire
-	IsPrivate    *bool                `bson:"is_private"`            // true = accès restreint
 	ChannelIDs   []primitive.ObjectID `bson:"channel_ids,omitempty"` // Liste des salons liés
 	CreatedAt    time.Time            `bson:"created_at"`
+}
+
+
+type ChatChannel struct {
+	ID              primitive.ObjectID   `bson:"_id,omitempty"`  // ID du salon
+	RoomID          primitive.ObjectID   `bson:"room_id"`  // id de la chatroom associée
+	Name            *string              `bson:"name"` 
+	IsPrivate       *bool                `bson:"is_private"` // est ce que le salon est privé ??
+	AllowedUserIDs  []primitive.ObjectID `bson:"allowed_user_ids,omitempty"` // user autorisés si il l'est 
+	CreatedAt       time.Time            `bson:"created_at"`
 }
