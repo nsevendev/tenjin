9b56b72bee8e8b402edf98a98db73c4d8d51c712
Alice <alice.cordier39210@gmail.com>
2025-07-29 16:24:37 +0200

    Refacto du Model Ressource et ajout de la constante TypeRessourceAssociated #7

--- back/internal/ressource/model.go
+++ back/internal/ressource/model.go
@@ -6,19 +6,15 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-// Model  d'une ressource
-type Model struct {
+type Ressource struct {
 	ID             primitive.ObjectID `bson:"_id" json:"id"`
 	URL            string             `bson:"url" json:"url" validate:"required,url"`
 	Name           string             `bson:"name" json:"name" validate:"required"`
-	Size           int64              `bson:"size" json:"size" validate:"required"`          // en octets
-	MimeType       string             `bson:"mime_type" json:"mimeType" validate:"required"` // ex: image/png
-	Type           string             `bson:"type" json:"type" validate:"required,oneof=pdf video img"`
-	AssociatedTo   primitive.ObjectID `bson:"associated_to" json:"associatedTo" validate:"required"`                                        // ID de l'entité liée
-	AssociatedType string             `bson:"associated_type" json:"associatedType" validate:"required,oneof=user session institute offer"` // Type de lien
+	Size           int64              `bson:"size" json:"size" validate:"required"`
+	MimeType       string             `bson:"mime_type" json:"mimeType" validate:"required"`
+	Type           string             `bson:"type" json:"type" validate:"required"`
+	AssociatedTo   primitive.ObjectID `bson:"associated_to" json:"associatedTo" validate:"required"`
+	AssociatedType string             `bson:"associated_type" json:"associatedType" validate:"required,oneof=user session institute offer quiz recruiter evaluation competence "`
 	UploadedBy     primitive.ObjectID `bson:"uploaded_by" json:"uploadedBy" validate:"required"`
 	CreatedAt      time.Time          `bson:"created_at" json:"createdAt" validate:"required"`
-	// ajouter la taille
-	// ajouter à qui ou quoi il a été associé
-	// le type : pdf, video image etc
 }
