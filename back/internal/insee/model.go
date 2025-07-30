fccb70e3b75189e90274442cbbd0b47a1bac4ad3
BoissonMathis <math.boiss@laposte.net>
2025-07-30 10:26:11 +0200

    add struct sireneResponse etablissement and unitelegale in insee model #27

--- back/internal/insee/model.go
+++ back/internal/insee/model.go
@@ -16,6 +16,24 @@ type CompanyInfo struct {
     CategorieJuridique      string `json:"categorie_juridique"`
 }
 
+type sireneResponse struct {
+	Etablissement sireneEtablissement `json:"etablissement"`
+}
+
+type sireneEtablissement struct {
+	Siret                 string                   `json:"siret"`
+	UniteLegale           sireneUniteLegale        `json:"uniteLegale"`
+	AdresseEtablissement  sireneAdresseEtablissement `json:"adresseEtablissement"`
+	Enseigne1Etablissement string                  `json:"enseigne1Etablissement"`
+}
+
+type sireneUniteLegale struct {
+	Siren                          string `json:"siren"`
+	DenominationUniteLegale        string `json:"denominationUniteLegale"`
+	ActivitePrincipaleUniteLegale  string `json:"activitePrincipaleUniteLegale"`
+	CategorieJuridiqueUniteLegale  string `json:"categorieJuridiqueUniteLegale"`
+}
+
 type sireneAdresseEtablissement struct {
 	NumeroVoieEtablissement        string `json:"numeroVoieEtablissement"`
 	TypeVoieEtablissement          string `json:"typeVoieEtablissement"`
