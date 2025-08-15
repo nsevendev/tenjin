55f321187d285f709ff4512d2ee259161a736302
BoissonMathis <math.boiss@laposte.net>
2025-08-15 10:35:25 +0200

    commit de la mort

--- back/internal/utils/r2app/r2app.go
+++ back/internal/utils/r2app/r2app.go
@@ -2,7 +2,6 @@ package r2app
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"log"
 
@@ -11,8 +10,8 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-func NewR2Client(bucketName, accountId, accessKeyId, accessKeySecret string) *s3.Client{
-  cfg, err := config.LoadDefaultConfig(context.TODO(),
+func NewR2Client(bucketName, accountId, accessKeyId, accessKeySecret string) *s3.Client {
+cfg, err := config.LoadDefaultConfig(context.TODO(),
     config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(accessKeyId, accessKeySecret, "")),
     config.WithRegion("auto"),
   )
@@ -20,21 +19,34 @@ func NewR2Client(bucketName, accountId, accessKeyId, accessKeySecret string) *s3
     log.Fatal(err)
   }
 
-    client := s3.NewFromConfig(cfg, func(o *s3.Options) {
-        o.EndpointResolver = s3.EndpointResolverFromURL(fmt.Sprintf("https://%s.r2.cloudflarestorage.com", accountId))
-    })
+  client := s3.NewFromConfig(cfg, func(o *s3.Options) {
+    o.EndpointResolver = s3.EndpointResolverFromURL(fmt.Sprintf("https://%s.r2.cloudflarestorage.com", accountId))
+  })
 
-  return client
+	return client
 }
 
-func (r *R2Client) ListBuckets() {
-    output, err := r.Client.ListBuckets(context.TODO(), &s3.ListBucketsInput{})
-    if err != nil {
-        log.Fatal(err)
-    }
 
-    for _, object := range output.Buckets {
-        obj, _ := json.MarshalIndent(object, "", "\t")
-        fmt.Println(string(obj))
-    }
+func ListObjects(client *s3.Client, bucketName string) {
+	output, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+		Bucket: &bucketName,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, object := range output.Contents {
+		fmt.Printf("%+v\n", object)
+	}
+}
+
+func ListBuckets(client *s3.Client) {
+	output, err := client.ListBuckets(context.TODO(), &s3.ListBucketsInput{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, bucket := range output.Buckets {
+		fmt.Printf("%+v\n", bucket)
+	}
 }
--- back/internal/utils/r2app/r2app_test.go
+++ back/internal/utils/r2app/r2app_test.go
@@ -21,3 +21,33 @@ func TestNewR2Client_success(t *testing.T) {
 
 	log.Printf("Client R2 cree avec succes : %#v\n", client)
 }
+
+func TestListObjects_success(t *testing.T) {
+	bucketName := env.Get("R2_BUCKET_NAME")
+	accountId := env.Get("R2_ACCOUNT_ID")
+	accessKeyId := env.Get("R2_ACCESS_KEY_ID")
+	accessKeySecret := env.Get("R2_ACCESS_KEY_SECRET")
+
+	client := NewR2Client(bucketName, accountId, accessKeyId, accessKeySecret)
+
+	if client == nil {
+		t.Fatal("Echec de la creation du client R2 : client est nil")
+	}
+
+	ListObjects(client, bucketName)
+}
+
+func TestListBuckets_success(t *testing.T) {
+	bucketName := env.Get("R2_BUCKET_NAME")
+	accountId := env.Get("R2_ACCOUNT_ID")
+	accessKeyId := env.Get("R2_ACCESS_KEY_ID")
+	accessKeySecret := env.Get("R2_ACCESS_KEY_SECRET")
+
+	client := NewR2Client(bucketName, accountId, accessKeyId, accessKeySecret)
+
+	if client == nil {
+		t.Fatal("Echec de la creation du client R2 : client est nil")
+	}
+
+	ListBuckets(client)
+}
