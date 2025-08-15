571ec5b681749f56d7a94f0cc7d9e02ad4ec0265
jha <j.haimez@vattenfall.fr>
2025-08-15 15:22:12 +0200

    feat: rajout de key manquantent dans les struct metier

--- back/internal/jobs/Dto.go
+++ back/internal/jobs/Dto.go
@@ -1,6 +1,9 @@
 package jobs
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
 
 // JobDTO Structure complète d'un métier ROME
 type JobDTO struct {
@@ -32,6 +35,13 @@ type JobDTO struct {
 	TransitionEcologique             bool                    `json:"transitionEcologique" bson:"transitionEcologique"`
 	TransitionEcologiqueDetaillee    string                  `json:"transitionEcologiqueDetaillee" bson:"transitionEcologiqueDetaillee"`
 	TransitionNumerique              bool                    `json:"transitionNumerique" bson:"transitionNumerique"`
+	Label                            string                  `json:"label" bson:"label"`
+	DateFin                          *time.Time              `json:"dateFin,omitempty" bson:"dateFin,omitempty"`
+	AppellationsEnvisageables        []AppellationSimpleDTO  `json:"appellationsEnvisageables" bson:"appellationsEnvisageables"`
+	MetiersEnvisageables             []MetierSimpleDTO       `json:"metiersEnvisageables" bson:"metiersEnvisageables"`
+	MetiersProches                   []MetierSimpleDTO       `json:"metiersProches" bson:"metiersProches"`
+	AppellationsProches              []AppellationSimpleDTO  `json:"appellationsProches" bson:"appellationsProches"`
+	MetiersEnProximite               []MetierSimpleDTO       `json:"metiersEnProximite" bson:"metiersEnProximite"`
 }
 
 // JobSummaryDTO Version résumée pour les listes
