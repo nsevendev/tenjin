56cb6869b983b6a7e476274b0cbbb22548375a17
jha <j.haimez@vattenfall.fr>
2025-08-15 22:49:16 +0200

    refacto: rename + changement chatroom

--- back/internal/chats/ChatModel.go
+++ back/internal/chats/ChatModel.go
@@ -2,6 +2,7 @@ package chats
 
 import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"tenjin/back/internal/utils/constantes"
 	"time"
 )
 
@@ -16,36 +17,22 @@ type ChatHub struct {
 	CreatedAt   time.Time            `bson:"created_at"`                       // Date de création
 }
 
-// Types de salon possibles (a voir pour cette partie)
-const (
-	ChannelTypeInfo      = "info"      // Salon d’info, pas de débat possible
-	ChannelTypePrivate   = "private"   // Salon privé, accès limité
-	ChannelTypeModerated = "moderated" // Salon modéré, messages contrôlés par des modérateurs
-	ChannelTypeRecruiter = "recruiter" // Salon spécial recruteur, avec auto-suppression après 1 mois
-)
-
-const (
-	StatusActive   = "active"
-	StatusArchived = "archived"
-	StatusDeleted  = "deleted"
-)
-
 // ChatChannel représente un salon de discussion dans un ChatHub
 type ChatChannel struct {
-	ID             primitive.ObjectID   `bson:"_id,omitempty"`
-	HubID          primitive.ObjectID   `bson:"hub_id" validate:"required"` // ID du hub auquel appartient ce salon
-	SessionID      *primitive.ObjectID  `bson:"session_id,omitempty"`       // Optionnel : lien vers une session formation, ou nil sinon
-	Name           *string              `bson:"name" validate:"required"`   // Nom du salon (ex : "Général", "Recrutement")
-	Type           *string              `bson:"type" validate:"required"`   // Type de salon (info, private, moderated, recruiter) - Obligatoire
-	IsPrivate      bool                 `bson:"is_private"`                 // Indique si le salon est privé (accès restreint) - facultatif
-	Participants   []primitive.ObjectID `bson:"participants,omitempty"`     // Liste des IDs des utilisateurs présents dans ce salon
-	Moderators     []primitive.ObjectID `bson:"moderators,omitempty"`       // Liste des modérateurs, si le salon est modéré
-	AllowedUserIDs []primitive.ObjectID `bson:"allowed_user_ids,omitempty"` // Liste des utilisateurs autorisés si salon privé (peut être vide sinon)
-	MessageIDs     []primitive.ObjectID `bson:"messages,omitempty"`         // Liste des IDs des messages envoyés dans ce salon
-	LastMessage    *LastMessagePreview  `bson:"last_message,omitempty"`     // le dernier message relié a la struct lastmessagepreview
-	CreatedAt      time.Time            `bson:"created_at"`                 // Date de création du salon
-	AutoDeleteAt   *time.Time           `bson:"auto_delete_at,omitempty"`   // Date de suppression automatique (ex : salon recruteur 1 mois)
-	Status         *string              `bson:"status" validate:"required"` // Statut du salon : active, archived ou deleted - Obligatoire
+	ID             primitive.ObjectID     `bson:"_id,omitempty"`
+	HubID          primitive.ObjectID     `bson:"hub_id" validate:"required"` // ID du hub auquel appartient ce salon
+	SessionID      *primitive.ObjectID    `bson:"session_id,omitempty"`       // Optionnel : lien vers une session formation, ou nil sinon
+	Name           *string                `bson:"name" validate:"required"`   // Nom du salon (ex : "Général", "Recrutement")
+	Type           *string                `bson:"type" validate:"required"`   // Type de salon (info, private, moderated, recruiter) - Obligatoire
+	IsPrivate      bool                   `bson:"is_private"`                 // Indique si le salon est privé (accès restreint) - facultatif
+	Participants   []primitive.ObjectID   `bson:"participants,omitempty"`     // Liste des IDs des utilisateurs présents dans ce salon
+	Moderators     []primitive.ObjectID   `bson:"moderators,omitempty"`       // Liste des modérateurs, si le salon est modéré
+	AllowedUserIDs []primitive.ObjectID   `bson:"allowed_user_ids,omitempty"` // Liste des utilisateurs autorisés si salon privé (peut être vide sinon)
+	MessageIDs     []primitive.ObjectID   `bson:"messages,omitempty"`         // Liste des IDs des messages envoyés dans ce salon
+	LastMessage    *LastMessagePreview    `bson:"last_message,omitempty"`     // le dernier message relié a la struct lastmessagepreview
+	CreatedAt      time.Time              `bson:"created_at"`                 // Date de création du salon
+	AutoDeleteAt   *time.Time             `bson:"auto_delete_at,omitempty"`   // Date de suppression automatique (ex : salon recruteur 1 mois)
+	Status         constantes.StatusState `bson:"status" validate:"required"` // Statut du salon : active, archived ou deleted - Obligatoire
 }
 
 // ChatMessage représente un message envoyé dans un ChatChannel
