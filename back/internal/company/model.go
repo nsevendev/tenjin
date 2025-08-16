e09f53c37e093c2d2b9c3106a21f207104657c86
BoissonMathis <math.boiss@laposte.net>
2025-08-16 08:28:43 +0200

    refacto struct companyCreateDto #33

--- back/internal/company/model.go
+++ back/internal/company/model.go
@@ -48,17 +48,16 @@ type CompanyRetrieveDto struct {
 }
 
 type CompanyCreateDto struct {
-	BusinessName  string               `bson:"business_name" json:"business_name" validate:"required"`
-	Siret         string               `bson:"siret" json:"siret" validate:"required"`
-	Siren         string               `bson:"siren" json:"siren" validate:"required"`
-	Sector        string               `bson:"sector" json:"sector" validate:"required"`
-	CompType      string               `bson:"comp_type" json:"comp_type" validate:"required"`
-	Address       string               `bson:"address" json:"address" validate:"required"`
-	ZipCode       string               `bson:"zip_code" json:"zip_code" validate:"required"`
-	City          string               `bson:"city" json:"city" validate:"required"`
-	ContactEmails []string             `bson:"contact_emails" json:"contact_emails"`
-	Formations    []primitive.ObjectID `bson:"formations" json:"formations"`
-	Users         []primitive.ObjectID `bson:"users" json:"users" validate:"required"`
+	BusinessName  string                   `bson:"business_name" json:"businessName" validate:"required,min=2,max=200"`
+	Siret         string                   `bson:"siret" json:"siret" validate:"required,len=14,numeric"`
+	Addresses     []addresses.Address      `bson:"addresses" json:"addresses" validate:"required,dive"`
+	ContactEmails []string                 `bson:"contact_emails" json:"contactEmails" validate:"required,min=1,dive,email"`
+	Phones        []phones.Phone           `bson:"phones" json:"phones" validate:"required,min=1,dive"`
+	Status        constantes.StatusState   `bson:"status" json:"status" validate:"required,oneof=enable disable suspended"`
+	Type          constantes.TypeInstitute `bson:"type" json:"type" validate:"required,oneof=public private association"`
+	LogoUrl       *string                  `bson:"logo_url" json:"logoUrl" validate:"omitempty,url"`
+	FormationIDs  []primitive.ObjectID     `bson:"formation_ids" json:"formationIds"`
+	UserIDs       []primitive.ObjectID     `bson:"user_ids" json:"userIds"`
 }
 
 func (c *Company) SetTimeStamps() {
