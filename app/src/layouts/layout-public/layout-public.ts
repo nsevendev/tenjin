49c10d17a0a5f68d8c7017092128f2e376d58388
jha <j.haimez@vattenfall.fr>
2025-08-31 20:41:26 +0200

    feat(#35): init layout private et public

---
+++ app/src/layouts/layout-public/layout-public.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-layout-public',
+  templateUrl: './layout-public.template.html',
+  imports: [
+    RouterOutlet
+  ],
+})
+export class LayoutPublic {
+}
