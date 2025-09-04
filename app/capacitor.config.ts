5788c3589c3a51712fd6c8d649da1581d57ba6ad
jha <j.haimez@vattenfall.fr>
2025-09-04 23:19:25 +0200

    feat : ajout front
    - ajout de capacitor pour mobile
    - ajout d'un interceptor qui set l'url de l'api via un fichier d'environment
    - ajout cors sur backend

---
+++ app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nseven.tenjin',
+  appName: 'Tenjin',
+  webDir: 'dist'
+};
+
+export default config;
