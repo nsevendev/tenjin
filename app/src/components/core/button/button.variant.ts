f719884f17574dd4d48e41470c98881b723b5ecd
jha <j.haimez@vattenfall.fr>
2025-09-05 20:00:39 +0200

    feat(#35): new system icon svg

--- app/src/components/core/button/button.variant.ts
+++ app/src/components/core/button/button.variant.ts
@@ -3,7 +3,6 @@ import { cva, type VariantProps } from 'class-variance-authority';
 export const buttonVariants = cva(
   [
     'inline-flex items-center justify-center rounded-lg font-semibold cursor-pointer',
-    'transition-all duration-150',
     'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2',
     'disabled:opacity-50 disabled:cursor-not-allowed',
     'active:scale-95 active:shadow-inner',
@@ -17,6 +16,9 @@ export const buttonVariants = cva(
         success:   'bg-success   text-white hover:opacity-90        focus-visible:ring-success',
         error:     'bg-error     text-white hover:opacity-90        focus-visible:ring-error',
         warning:   'bg-warning   text-white hover:opacity-90        focus-visible:ring-warning',
+        transparentDefault: 'bg-transparent hover:underline',
+        transparentDefaultPrimary: 'bg-transparent hover:text-primary hover:underline        focus-visible:ring-primary',
+        transparentPrimary: 'bg-transparent text-primary hover:underline        focus-visible:ring-primary',
 
         // avec opacity
         primaryOpacity:   'bg-primary/15   text-primary   hover:bg-primary/25   focus-visible:ring-primary',
@@ -25,9 +27,13 @@ export const buttonVariants = cva(
         errorOpacity:     'bg-error/15     text-error     hover:bg-error/25     focus-visible:ring-error',
         warningOpacity:   'bg-warning/15   text-warning   hover:bg-warning/25   focus-visible:ring-warning',
       },
+      textSize: {
+        sm:   'text-xs md:text-sm',
+        base: 'text-sm md:text-base',
+      },
       size: {
-        sm:   'px-3 py-2 text-xs md:px-4 md:text-sm',
-        base: 'px-4 py-3 text-sm md:px-6 md:text-base',
+        sm:   'px-3 py-2 md:px-4',
+        base: 'px-4 py-3 md:px-6',
       },
       transform: {
         default: '',
@@ -37,6 +43,7 @@ export const buttonVariants = cva(
     defaultVariants: {
       variant: 'primary',
       size: 'base',
+      textSize: 'base',
       transform: 'default',
     },
     compoundVariants: [],
---
+++ app/src/components/core/icon-svg/hericon/hericon.component.spec.ts
@@ -0,0 +1,16 @@
+import {TestBed} from '@angular/core/testing';
+import {ContainerComponent} from '../../container/container.component';
+import {provideZonelessChangeDetection} from '@angular/core';
+
+describe('ContainerComponent', () => {
+  let iconComponent: Icon
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [],
+      providers: []
+    })
+      .compileComponents()
+
+
+  })
+})
