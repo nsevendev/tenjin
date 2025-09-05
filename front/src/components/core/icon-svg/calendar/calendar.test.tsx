8166a710d03015ddac6331eab409f777a0f3934f
jha <j.haimez@vattenfall.fr>
2025-09-05 11:04:56 +0200

    delete old folder project qwik

--- front/src/components/core/icon-svg/calendar/calendar.test.tsx
+++
@@ -1,9 +0,0 @@
-import { test, expect, describe } from "vitest";
-import {CalendarIcon} from "~/components/core/icon-svg/calendar/calendar";
-
-describe('CalendarIcon', () => {
-    test('le composant est défini et exporté', () => {
-        expect(CalendarIcon).toBeDefined();
-        expect(typeof CalendarIcon).toBe('function');
-    });
-});
--- front/src/components/core/icon-svg/document-text/document-text.test.tsx
+++
@@ -1,9 +0,0 @@
-import { test, expect, describe } from "vitest";
-import {DocumentTextIcon} from "~/components/core/icon-svg/document-text/document-text";
-
-describe('DocumentTextIcon', () => {
-    test('le composant est défini et exporté', () => {
-        expect(DocumentTextIcon).toBeDefined();
-        expect(typeof DocumentTextIcon).toBe('function');
-    });
-});
